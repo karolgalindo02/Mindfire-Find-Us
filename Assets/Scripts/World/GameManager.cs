f2734b862e059901545435d35ed2ae9b79f470e1
Jorge Luis Navarro P. <jorgluisnavarro@gmail.com>
2024-08-13 14:16:42 -0500

    Grenade logic added and effects

--- Assets/Scripts/World/GameManager.cs
+++ Assets/Scripts/World/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,10 +8,22 @@ public class GameManager : MonoBehaviour
     //Singleton pattern
     public static GameManager Instance { get; private set; }
 
+    //Canvas UI Ammo
+    public TextMeshProUGUI ammoText;
+    public TextMeshProUGUI healthText;
+
+    public int health = 100;
+
     public int ammo = 10;
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Update()
+    {
+        ammoText.text = ammo.ToString();
+        healthText.text = health.ToString();
     }
 }
