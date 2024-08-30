02dbc66a1aebf3690a8a9080b8d4adc0cbc9b6df
Jorge Luis Navarro P. <jorgluisnavarro@gmail.com>
2024-08-30 01:50:49 -0500

    Health bar enemies added

--- Assets/Scripts/AI_Enemies/Enemy.cs
+++ Assets/Scripts/AI_Enemies/Enemy.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float health = 50;
+    [SerializeField] private float maxHealth = 50;
+    [SerializeField] private float currentHealth;
+    [SerializeField] private Image healthBar;
 
+    public void Start()
+    {
+        currentHealth = maxHealth;
+        
+        UpdateHealthBar();
+    }
     public void TakeDamage(int damage)
     {
-        health -= damage;
-        if(health <= 0)
+        currentHealth -= damage;
+
+        UpdateHealthBar();
+
+        if(currentHealth <= 0)
         {
             Die();
         }
     }
+
+    private void UpdateHealthBar()
+    {
+        healthBar.fillAmount = currentHealth / maxHealth;
+    }
+
 
     private void Die()
     {
